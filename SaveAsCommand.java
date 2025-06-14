82fa5dc34aaf37326a7db5d54186c7918b288238
Borislav04 <borkolaka@gmail.com>
2025-06-14 17:03:18 +0300

    Create SaveAsCommand.java

---
+++ SaveAsCommand.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class SaveAsCommand implements Command {
+    private FileManager fileManager = new FileManager();
+
+    @Override
+    public void execute(ApplicationContext context, String[] args) {
+        if (!context.isFileOpen()) {
+            System.out.println("Няма отворен файл.");
+            return;
+        }
+
+        if (args.length < 2) {
+            System.out.println("Употреба: saveas <път до нов файл>");
+            return;
+        }
+
+        File newFile = new File(args[1]);
+        fileManager.saveUniversityToFile(newFile, context.getUniversity());
+        context.setCurrentFile(newFile);
+    }
+}

75ecb86d4f29755ac39b292f75f0303ad10c2b16
Borislav04 <borkolaka@gmail.com>
2025-06-14 17:02:54 +0300

    Create SaveCommand.java

---
+++ SaveCommand.java
@@ -0,0 +1,13 @@
+public class SaveCommand implements Command {
+    private FileManager fileManager = new FileManager();
+
+    @Override
+    public void execute(ApplicationContext context, String[] args) {
+        if (!context.isFileOpen()) {
+            System.out.println("Няма отворен файл за запис.");
+            return;
+        }
+
+        fileManager.saveUniversityToFile(context.getCurrentFile(), context.getUniversity());
+    }
+}

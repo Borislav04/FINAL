d0fddc28550841e8acd602d4c2eb8b70d872f0cf
Borislav04 <borkolaka@gmail.com>
2025-06-14 19:29:01 +0300

    Create AdvanceCommand.java

---
+++ AdvanceCommand.java
@@ -0,0 +1,25 @@
+/**
+ * Команда за преминаване на студент в следващ курс.
+ * Употреба: advance <фн>
+ */
+public class AdvanceCommand implements Command {
+    @Override
+    public void execute(ApplicationContext context, String[] args) {
+        if (!context.isFileOpen()) {
+            System.out.println("Няма отворен файл. Използвайте 'open' първо.");
+            return;
+        }
+
+        if (args.length < 2) {
+            System.out.println("Употреба: advance <фн>");
+            return;
+        }
+
+        try {
+            int fn = Integer.parseInt(args[1]);
+            context.getUniversity().advanceStudent(fn);
+        } catch (NumberFormatException e) {
+            System.out.println("Невалиден формат за факултетен номер.");
+        }
+    }
+}

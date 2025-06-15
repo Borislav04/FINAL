cb640024b9db18247de52fde0da270791fc9e6f1
Borislav04 <borkolaka@gmail.com>
2025-06-15 13:12:38 +0300

    Create ResumeCommand.java

---
+++ ResumeCommand.java
@@ -0,0 +1,25 @@
+/**
+ * Команда за възстановяване на студент.
+ * Употреба: resume <фн>
+ */
+public class ResumeCommand implements Command {
+    @Override
+    public void execute(ApplicationContext context, String[] args) {
+        if (!context.isFileOpen()) {
+            System.out.println("Няма отворен файл. Използвайте 'open' първо.");
+            return;
+        }
+
+        if (args.length < 2) {
+            System.out.println("Употреба: resume <фн>");
+            return;
+        }
+
+        try {
+            int fn = Integer.parseInt(args[1]);
+            context.getUniversity().resumeStudent(fn);
+        } catch (NumberFormatException e) {
+            System.out.println("Невалиден факултетен номер.");
+        }
+    }
+}

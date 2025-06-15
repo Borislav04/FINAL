3099a25666e9104ec08976ae540ca0624efff303
Borislav04 <borkolaka@gmail.com>
2025-06-15 13:09:55 +0300

    Create ChangeCommand.java

---
+++ ChangeCommand.java
@@ -0,0 +1,38 @@
+/**
+ * Команда за промяна на информация за студент.
+ * Употреба: change <фн> <program|group|year> <стойност>
+ */
+public class ChangeCommand implements Command {
+    @Override
+    public void execute(ApplicationContext context, String[] args) {
+        if (!context.isFileOpen()) {
+            System.out.println("Няма отворен файл. Използвайте 'open' първо.");
+            return;
+        }
+
+        if (args.length < 4) {
+            System.out.println("Употреба: change <фн> <program|group|year> <стойност>");
+            return;
+        }
+
+        try {
+            int fn = Integer.parseInt(args[1]);
+            String option = args[2];
+            String value = args[3];
+
+            switch (option.toLowerCase()) {
+                case "program" -> context.getUniversity().changeStudentProgram(fn, value);
+                case "group" ->
+                        context.getUniversity().findStudentByFacultyNumber(fn).ifPresent(s -> s.setGroup(value));
+                case "year" -> {
+                    int year = Integer.parseInt(value);
+                    context.getUniversity().findStudentByFacultyNumber(fn).ifPresent(s -> s.setYear(year));
+                }
+                default -> System.out.println("Невалидна опция. Използвайте 'program', 'group' или 'year'.");
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Невалиден формат за факултетен номер или година.");
+        }
+    }
+}

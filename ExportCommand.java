f8efd23c1eb092ef36813c7e2ab81bce68495753
Borislav04 <borkolaka@gmail.com>
2025-06-15 22:49:32 +0300

    Create ExportCommand.java

---
+++ ExportCommand.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+/**
+ * Команда за експортиране на студентски данни в текстов файл.
+ * Употреба: export [име на файл]
+ */
+public class ExportCommand implements Command {
+    private FileManager fileManager = new FileManager();
+
+    @Override
+    public void execute(ApplicationContext context, String[] args) {
+        if (!context.isFileOpen()) {
+            System.out.println("Няма отворен файл. Моля, използвайте 'open' първо.");
+            return;
+        }
+
+        String filename = (args.length > 1) ? args[1] : "students_report.txt";
+        fileManager.exportToTextFile(filename, context.getUniversity());
+    }
+}

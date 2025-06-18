a21ce3404cc16ecd6daf4dd5f92e9a5d86e55df3
Borislav04 <borkolaka@gmail.com>
2025-06-18 10:36:56 +0300

    Update ResumeCommand.java

--- ResumeCommand.java
+++ ResumeCommand.java
@@ -18,6 +18,7 @@ public class ResumeCommand implements Command {
         try {
             int fn = Integer.parseInt(args[1]);
             context.getUniversity().resumeStudent(fn);
+            context.setModified(true);
         } catch (NumberFormatException e) {
             System.out.println("Невалиден факултетен номер.");
         }
